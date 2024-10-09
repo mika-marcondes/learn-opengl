952f08f742410704351d16cc0bfbf73be9921120
mikael <mika.marcondes2@gmail.com>
2024-10-09 13:54:45 -0300

    TRIANGLE!1!!

--- src/main.cpp
+++ src/main.cpp
@@ -63,9 +63,21 @@ int main()
   };
 
   unsigned int VBO;
+  unsigned int VAO;
+
+  glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
   const unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
@@ -98,6 +110,9 @@ int main()
     processInput(window);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
