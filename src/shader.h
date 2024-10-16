7556384daedab12c022c6df8fe43902cf3db0632
Mikael <mika.marcondes2@gmail.com>
2024-10-16 15:08:10 -0300

    🧹 wip: compile shaders & check for errors

--- src/shader.h
+++ src/shader.h
@@ -38,11 +38,41 @@ public:
         fragmentCode = fShaderStream.str();
       } catch (std::ifstream::failure& e)
       {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ\n" << e.what() << std::endl;
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
       }
       const char* vShaderCode = vertexCode.c_str();
       const char* fShaderCode= fragmentCode.c_str();
+
+      unsigned int vertex, fragment;
+      // compile shaders
+      vertex = glCreateShader(GL_VERTEX_SHADER);
+      glShaderSource(vertex, 1, &vShaderCode, nullptr);
+      glCompileShader(vertex);
+      checkCompileErrors(vertex, "VERTEX");
+
+      fragment = glCreateShader(GL_FRAGMENT_SHADER);
+      glShaderSource(fragment, 1, &fShaderCode, nullptr);
+      glCompileShader(fragment);
+      checkCompileErrors(fragment, "FRAGMENT");
     }
+
+    void use();
+
+private:
+  void checkCompileErrors(unsigned int shader, std::string type)
+  {
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM")
+    {
+      glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+      if (!success)
+      {
+        glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+        std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n" << std::endl;
+      }
+    }
+  }
 };
 
 #endif // SHADER_H
