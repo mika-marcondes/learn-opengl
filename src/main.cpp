18c873f8ac9d88926ed836fc8bfa913ac34c3342
Mikael <mika.marcondes2@gmail.com>
2024-10-16 14:17:48 -0300

    🧹 wip: bind vertices

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,23 @@ int main()
     0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, // top center
   };
 
+  unsigned int VBO, VAO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
 
   while (!glfwWindowShouldClose(window))
   {
@@ -68,8 +85,6 @@ int main()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
